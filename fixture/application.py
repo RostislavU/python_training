9999b3f205344e9cef7c704487ff52cd0f642f80
RostislavU <rostislav.udalov@yandex.ru>
2018-03-18 09:07:52 +0300

    Выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-
+from fixture.session import SessionHelper
 
 class Application:
 
@@ -7,6 +7,7 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False},
                             firefox_binary="F:/Mozilla/FirefoxPortableESR/App/Firefox64/firefox.exe")
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -109,23 +110,6 @@ class Application:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    #login/logout
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
 
     def destroy(self):
