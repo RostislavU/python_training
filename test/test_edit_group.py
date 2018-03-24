eb0fb8e595b73ff1fe438f1830c5ca1497db0288
RostislavU <rostislav.udalov@yandex.ru>
2018-03-24 10:14:09 +0300

    Включение вариативности изменяемых параметров при редактировании группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
+def test_edit_first_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="newName", header="newHeader", footer="newFooter"))
+    app.group.edit_first_group(Group(name="newName"))
+    app.session.logout()
+
+
+def test_edit_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="NewHeader"))
     app.session.logout()
