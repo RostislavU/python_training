88908d49da65803fd00a378396772db16e40d1b6
r.udalov <rostislav.udalov@yandex.ru>
2018-04-04 17:30:07 +0300

    Добавление обеспечения предусловий по части авторизации

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,7 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="F:/Mozilla/FirefoxPortableESR/App/Firefox64/firefox.exe")
+                            firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
