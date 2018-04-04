655809d243d4148feae07c7dcd0d24cc561e7a1b
r.udalov <rostislav.udalov@yandex.ru>
2018-04-04 22:14:20 +0300

    Добавлена проверка на наличие хотя бы одного элемента, требуемого для выполнения тестов

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,8 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="thisGroupWillBeDeleted"))
     old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="newName"))
     new_groups = app.group.get_group_list()
@@ -9,6 +11,8 @@ def test_edit_first_group_name(app):
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="thisGroupWillBeDeleted"))
     old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="NewHeader"))
     new_groups = app.group.get_group_list()
