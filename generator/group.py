5b33de7e9847432ea45cd7e217fd0ffc1912acbf
RostislavU <rostislav.udalov@yandex.ru>
2018-04-20 23:20:46 +0300

    Генерация данных групп в файл

--- generator/group.py
+++ generator/group.py
@@ -4,6 +4,24 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of group", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
@@ -11,14 +29,13 @@ def random_string(prefix, maxlen):
 
 
 testdata = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name_", 10)]
-    for header in ["", random_string("header_", 10)]
-    for footer in ["", random_string("footer_", 10)]
+    Group(name=random_string("name_", 10), footer=random_string("footer_", 15),
+          header=random_string("header", 10))
+    for i in range(n)
 ]
 
 
-file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../data/group.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
